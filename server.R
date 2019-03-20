9555eff70b3679184588d47a7ecba816f2b8a772
Jeff Qiu <zqiu@uchicago.edu>
2019-03-20 17:53:48 -0500

    finished project

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(rgdal)
 library(tmap)
 library(tidyverse)
+library(ncf)
 
 # load data
 nepal <- readOGR("nepal", "nepal")
@@ -14,20 +15,25 @@ normalize <- function(df) {
   return ((df-min(df))/(max(df)-min(df)))
 }
 
-# preprocessing data
-factors <- data.frame(water = normalize(nepal$nosafh20), 
-                      hunger = normalize(nepal$malkids), 
-                      literacy = normalize(nepal$ad_illit))
-
+# normalized variables
+v <- data.frame(water = normalize(nepal$nosafh20), 
+                hunger = normalize(nepal$malkids), 
+                literacy = normalize(nepal$ad_illit))
 income <- 1-normalize(as.numeric(nepal$pcincmp))
 
+# find center of each region and covert SpatialPolygon to SpatialPointsDataFrame
+nepal.cent <- gCentroid(nepal,byid = TRUE)
+nepal.cent <- SpatialPointsDataFrame(nepal.cent@coords, data.frame(income, v), proj4string = nepal.cent@proj4string)
+
+
+### Server logic
 server <- function(input, output) {
   
   output$povIdxMap <- renderPlot({
     dinominator <- input$c_water+input$c_hunger+input$c_literacy
-    povidx <- (input$c_water*factors$water
-               +input$c_hunger*factors$hunger
-               +input$c_literacy*factors$literacy)/dinominator
+    povidx <- (input$c_water*v$water
+               +input$c_hunger*v$hunger
+               +input$c_literacy*v$literacy)/dinominator
     nepal$predictedpvt<- abs(income - povidx)*100
     povertyIdxMap <- 
       tm_shape(nepal) + 
@@ -36,14 +42,33 @@ server <- function(input, output) {
                 title.position = c('right', 'top'), 
                 legend.position = c('left', 'bottom'),frame = FALSE) +
       tm_fill("predictedpvt", palette = "Blues", style = "fixed", breaks=c(0, 10, 20, 30, 40), title = "Percentage %")
+    
     output$povIdxScore <- renderText({
       paste("<p><b>Overall prediction accuracy: ", round(100-(mean(nepal$predictedpvt)/mean(povidx)), 2), '%</b></p>')
     })
     
     povertyIdxMap
-    
   })
   
+  output$mapData <- renderPlot({
+    nepal$flex_data <- v[[input$var]]
+    tm_shape(nepal) +
+      tm_fill('flex_data', palette = "Blues", style = "jenks", title = "Value") +
+      tm_borders(alpha=0.3) +
+      tm_layout(title= input$var, title.position = c('right', 'top'), legend.position = c('left', 'bottom'),frame = FALSE)
+  })
+  
+  output$LISAcorrelation <- renderPlot({
+
+    lisa_result <- lisa(nepal.cent@coords[,1],nepal.cent@coords[,2], nepal.cent@data[[input$var]], neigh=4)
+
+    nepal$lisa_correlation <- ifelse (abs(lisa_result$p)<0.05, lisa_result$correlation ,NaN)
+
+    tm_shape(nepal) +
+      tm_fill("lisa_correlation", palette = "Spectral", style = "jenks", title = "Correlation", midpoint=0) +
+      tm_borders(alpha=0.3) +
+      tm_layout(title= 'LISA Cluster Map', title.position = c('right', 'top'), legend.position = c('left', 'bottom'),frame = FALSE)
+  })
   
 }
 
--- ui.R
+++ ui.R
@@ -1,19 +1,55 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
 fluidPage(
   
   # include the MathJax template
-  includeScript(path = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML"),
+  # includeScript(path = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML"),
   
-  # App title ----
-  titlePanel("SOSC 13220 Final Project"),
+  # App title
+  titlePanel("SOSC 13220 Final Project - Poverty in Nepal"),
   
-  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+      
+      # Sidebar panel for inputs
+      sidebarPanel(
+        
+        withTags({
+          div(class="header", checked=NA,
+              h3("Spatial Autocorrelation"),
+              p("Select variable to perform local spatial correlation analysis.")
+          )
+        }),
+        
+        radioButtons("var", "Variable", c("Water Access" = "water", 
+                                          "Literacy" = "literacy",
+                                          "Food Access" = "hunger"))
+      ),
+      
+      mainPanel(
+        
+        tabsetPanel(type = "tabs",
+                    tabPanel("LISA", plotOutput('LISAcorrelation')),
+                    tabPanel("Data", plotOutput("mapData"))
+        ),
+        # Output: Method 2
+        
+        
+        withTags({
+          div(p("Eliminating the possibility of false positive (p<0.05), the maps show that a few areas display strong spatial 
+                autocorrelation. The qualitative reason for this spatial distribution remain inconclusive, but the correlation does not 
+                seem to be related to the data. As the data visualizations show, the spatially correlated clusters has different data 
+                accross the regions."))
+        })
+      )
+        
+  ),
+  
+  # Sidebar layout with input and output definitions
   sidebarLayout(
     
-    # Sidebar panel for inputs ----
+    # Sidebar panel for inputs
     sidebarPanel(
+      
       withTags({
         div(class="header", checked=NA,
             h3("Understanding Poverty"),
@@ -21,19 +57,22 @@ fluidPage(
         )
       }),
       
-      # Input: Slider for the number of bins ----
+      # Input: Sliders
       sliderInput(inputId = "c_water", label = "Percentage population without safe water:", min = 0, max = 5, value = 1.3, step = 0.1),
       sliderInput(inputId = "c_hunger", label = "Percentage of children under age five who are malnourished:", min = 0, max = 5, value = 0.7, step = 0.1),
       sliderInput(inputId = "c_literacy", label = "Adult illiteracy rate:", min = 0, max = 5, value = 3.9, step = 0.1)
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
+      
       
 
+    ),
+    
+    # Main panel for displaying outputs
+    mainPanel(
       
-      # Output: Histogram ----
-      plotOutput("povIdxMap"),
+      
+      # Output: Method 1
+      tabsetPanel(type = "tabs",
+        tabPanel("Map", plotOutput("povIdxMap"))),
       
       htmlOutput("povIdxScore"),
       
@@ -41,14 +80,14 @@ fluidPage(
         div(p("Prediction is calculated by the weighted average of three variables.
               Accuracy is calculated as the percentage difference between prediction and actual income. 
               All variables are normalized through feature scaling."),
-            p('The most accurate weight combination for the three variables are (1.3, 0.7, 3.9), with an overall accuracy of 60.37%.
+            p('The most accurate weight combination for the three variables are (0.5, 1.3, 3.7), with an overall accuracy of 61.11%.
               While a single linear model is hardly accurate, it does provide a perspective between these variables: education
               is a better indicator of income in Nepal than food or water access. A plausible explaination may be that income level can
               increase drastically with better education, while improvement in basic needs may lead to decrease in poverty but no significant
               increase in income level.'),
-            p('For a better non-spatial model, a deep neural network could be helpful when making prediction across multiple variables. 
-              This specific dataset, however, may not have enough observations to support a training set.'))
-        # div(p("1.3, 0.7, 3.9"))
+            p('The model also provide insight into the regional differences. In some regions, giving one factor more wright can leads to 
+              better prediction of these regions but worse in others, which may be an indication that the factor plays a more significance 
+              role in poverty in these areas than other.'))
       })
       
     )
